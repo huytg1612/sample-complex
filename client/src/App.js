f5e20c26ecff472bb7c45ca5187c52d9d17cac04
huytg1612 <huytg1612@gmail.com>
2023-07-04 22:22:04 +0700

    update view

--- client/src/App.js
+++ client/src/App.js
@@ -19,7 +19,7 @@ function App() {
           >
             Learn React
           </a>
-          <Link to="/">Home</Link>
+          <Link to="/">Levi Home</Link>
           <Link to="/otherpage">Other Page</Link>
         </header>
         <div>
